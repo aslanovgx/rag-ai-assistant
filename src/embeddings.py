6575b6d016daf6a66d9d51545ca6407d2b0da83d
Mustafa Aslanov <aslanov7509@gmail.com>
2026-03-13 01:48:52 +0400

    add embeddings module

---
+++ src/embeddings.py
@@ -0,0 +1,11 @@
+from langchain_openai import OpenAIEmbeddings
+
+
+def get_embeddings():
+    """
+    Initialize embedding model.
+    """
+
+    embeddings = OpenAIEmbeddings()
+
+    return embeddings
