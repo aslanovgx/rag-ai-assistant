f015f9d7d9e16ce597e587f52b125a3746626111
Mustafa Aslanov <aslanov7509@gmail.com>
2026-03-13 04:26:49 +0400

    change embedding model to sentence-transformers

--- src/embeddings.py
+++ src/embeddings.py
@@ -1,4 +1,5 @@
-from langchain_openai import OpenAIEmbeddings
+# from langchain_openai import OpenAIEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
 
 def get_embeddings():
@@ -6,6 +7,10 @@ def get_embeddings():
     Initialize embedding model.
     """
 
-    embeddings = OpenAIEmbeddings()
+    # embeddings = OpenAIEmbeddings()
+    
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
 
     return embeddings
