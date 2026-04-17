0807f178bd1aad7c14b95c617affcf5a25b5d013
Mustafa Aslanov <aslanov7509@gmail.com>
2026-04-17 22:42:46 +0400

    requirements.txt is updated

--- backend/cors_config.py
+++ backend/cors_config.py
@@ -1,12 +1,17 @@
 from fastapi.middleware.cors import CORSMiddleware
 
 
-from fastapi.middleware.cors import CORSMiddleware
-
 def setup_cors(app):
+    """
+    Configure CORS middleware for the FastAPI app.
+    """
+
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],
+        allow_origins=[
+            "http://localhost:3000",
+            "https://rag-ai-assistant-c6f9.vercel.app",
+        ],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
